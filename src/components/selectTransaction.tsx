82313343c2abdbfc5ba34b938e4d670602498275
juanpa77 <juanpa.box.77@gmail.com>
2022-03-29 11:22:43 -0300

    Add button toggle transaction type and finish transaction UI

--- src/components/selectTransaction.tsx
+++ src/components/selectTransaction.tsx
@@ -1,10 +1,25 @@
+import { MouseEvent, useState } from "react"
 
 
 export const SelectTransactionType = ()=> {
+    const [toggle, setToggle] = useState(false);
+    
+    const triggerToggle = (e: MouseEvent<HTMLDivElement>)=> {
+        const classList = e.currentTarget.classList.length;        
+        if (classList < 2) {
+            setToggle(!toggle)
+        }
+    }
+
     return (
         <div className="select__radio">
-            <div className="icome__radioButton">Ingreso</div>
-            <div className="expense__radioButton active__radioButton">Gasto</div>
+            <div className={`icome__radioButton ${toggle ? 'active__radioButton': ''}`}
+            onClick={(e)=>triggerToggle(e)}
+            >Ingreso</div>
+            <div onClick={(e)=>triggerToggle(e)}
+            className= {`expense__radioButton ${toggle ? '': 'active__radioButton'}`}>
+                Gasto
+            </div>
         </div>
     )
-}
+}/*  */
