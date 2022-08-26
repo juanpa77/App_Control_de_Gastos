ed5b83444d4219b9afa8712803b53c80775b6c17
Juan77 <juanpa.box.77@gmail.com>
2022-08-26 15:27:48 -0300

    add btn for login  with google, add obverver to  user login for persist the session

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,12 +1,35 @@
+import { NextOrObserver, User } from "firebase/auth"
+import { useEffect } from "react"
 import { Base } from "../../components/buttons/styled"
+import { setActiveUser } from "../../features/user/userSlice"
+import { useAppDispatch, useAppSelector } from "../../hooks/useAuth"
+import { observerAuth, signWithGoogle } from "../../services/authProvider"
 import { Wrapper } from "./styled"
 
 const Login = () => {
+  const loguin = useAppSelector(state => state)
+  const dispatch = useAppDispatch()
+
+  const isAuth: NextOrObserver<User> = user => {
+    user && dispatch(setActiveUser({
+      loading: true,
+      userInfo: {
+        email: user.displayName,
+        name: user.email
+      },
+      userToken: user.uid
+    }))
+  }
+
+  useEffect(() => {
+    observerAuth(isAuth)
+    console.log(loguin)
+  }, [loguin])
 
   return (
     <Wrapper>
       <em>Login with Google</em>
-      <Base />
+      <Base onClick={() => signWithGoogle()} />
     </Wrapper>
   )
 }
