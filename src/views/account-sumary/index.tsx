c1f8ff957427fbbfd0117eac178842c91636e24d
Juan77 <juanpa.box.77@gmail.com>
2022-09-01 13:06:54 -0300

    applied restore

--- src/views/account-sumary/index.tsx
+++ src/views/account-sumary/index.tsx
@@ -1,13 +1,17 @@
 import { ReactComponent as Config } from "../../components/BalanceCard/assets/config.svg";
+import { ReactComponent as User } from "../../asset/icons/user.svg"
 import { Idb } from "../../utility/IDB";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import { useCategoryContex } from "../../hooks/useContex";
 import { Balance } from "../../components/BalanceCard";
 import { Footer, Wrapper } from "./styled";
+import UserModal from "./userModal";
+import { useModal } from "../../hooks/useModal";
 
 export const AccountSumary = ({ db }: { db: Idb }) => {
   const { setCategory } = useCategoryContex();
+  const [isOpenModal, openModal, closeModal] = useModal(false)
 
   useEffect(() => {
     db.config.getCategory().then((res) => setCategory(res));
@@ -16,6 +20,7 @@ export const AccountSumary = ({ db }: { db: Idb }) => {
   return (
     <Wrapper>
       <Footer>
+        <User onClick={openModal} />
         Estado de Cuenta
         <Link to={"/config"}>
           <Config />
@@ -24,6 +29,7 @@ export const AccountSumary = ({ db }: { db: Idb }) => {
       <Balance db={db} dateRanges="Mensual" />
       <Balance db={db} dateRanges="Semanal" />
       <Balance db={db} dateRanges="Diario" />
+      <UserModal isOpenModal={isOpenModal} closeModal={closeModal} />
     </Wrapper>
   );
 };
